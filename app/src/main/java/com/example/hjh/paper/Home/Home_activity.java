a31c5ca4cae325d9310db5ab7c56e2c7e7ec6b63
HJH <785227331@qq.com>
2016-05-12 17:45:02 +0800

    添加了底部导航栏

--- app/src/main/java/com/example/hjh/paper/Home/Home_activity.java
+++ app/src/main/java/com/example/hjh/paper/Home/Home_activity.java
@@ -1,10 +1,14 @@
 package com.example.hjh.paper.Home;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.hjh.paper.Home.Download.Download_Control;
@@ -20,15 +24,55 @@ public class Home_activity extends AppCompatActivity{
     private ArrayList<String> title_list = new ArrayList<>();
     private long exitTime = 0;//最后一次点击返回键的时间
     private static ViewPager v ;
+    private  ViewPager viewPager;
+
+    private Button home;
+    private Button download;
+    private Button set;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        init();
         initViewPage();
     }
 
+    private void init() {
+        home = (Button) findViewById(R.id.btn_home);
+        download = (Button) findViewById(R.id.btn_down);
+        set = (Button) findViewById(R.id.btn_set);
+        Drawable drawable1 = getResources().getDrawable(R.drawable.icon_set_normal);
+        drawable1.setBounds(0, 0, 40, 40);
+        home.setCompoundDrawables(null, drawable1, null, null);
+        download.setCompoundDrawables(null, drawable1, null, null);
+        set.setCompoundDrawables(null, drawable1, null, null);
+        //设置点击事件
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Home_activity.setposition(0);
+                Log.i("abc","ssssss");
+            }
+        });
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Home_activity.setposition(1);
+                Log.i("abc","ssssss");
+            }
+        });
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Home_activity.setposition(2);
+                Log.i("abc","ssssss");
+            }
+        });
+    }
+
     private void initViewPage() {
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpage_home);
+        viewPager = (ViewPager) findViewById(R.id.viewpage_home);
         v = viewPager;
         fragmentslist = new ArrayList<>();
 
@@ -94,6 +138,5 @@ public class Home_activity extends AppCompatActivity{
     public static void setposition(int position){
         v.setCurrentItem(position);
     }
-
 
 }
